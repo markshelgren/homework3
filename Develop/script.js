fba0b786eba3f5a46681047deb2f2578e8036582
markshelgren <67986616+markshelgren@users.noreply.github.com>
2020-09-26 19:18:23 -0700

    Created generatePassword function and trying to get rid of favicon load error message

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+	passwordText.value = password;
+}
 
+// Assignment code called this function in the provided writePassword function
+// but it did not exist.  Created it here and returned the calculated password
+function generatePassword(password) {
+	var password = "";
+	password = "XXXYYYZZZ000!@#";
+	return password;
 }
 
 // Add event listener to generate button
